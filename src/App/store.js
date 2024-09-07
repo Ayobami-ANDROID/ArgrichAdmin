861ee00c7d42447f77fbaec0b1c842329bbd58fb
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-09-07 18:28:34 +0100

    createing Slice

--- src/App/store.js
+++ src/App/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../../src/features/auth/authSlice";
 import ProductReducer from "../../src/features/product/productSlice"
+import categoryReducer from "../../src/features/category/categorySlice"
 
 
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    product: ProductReducer
+    product: ProductReducer,
+    categoty: categoryReducer
   },
 });
 
