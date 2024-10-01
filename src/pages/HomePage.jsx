fa1569d7da8bd09f2fe60d2ff3ab293d2e27ff34
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-10-01 23:12:34 +0100

    changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import secureLocalStorage from 'react-secure-storage'
 import apiClient from '../app/axiosConfig'
 import { toast } from 'react-toastify'
@@ -12,12 +13,18 @@ const HomePage = () => {
   const [isLoading, setisLoading] = useState(false)
   const name = secureLocalStorage.getItem("name")
   const email = secureLocalStorage.getItem("email")
+  const [customer, setCustomers] = useState([])
 
-  const label=["Delivered Orders", "UnDelivered Orders"]
+  const label = ["Delivered Orders", "UnDelivered Orders"]
   const series = [data.deliveredOrders, data.undeliveredOrders]
 
   useEffect(() => {
     setisLoading(true)
+    fetchData()
+    fetchData2()
+  }, [])
+
+  const fetchData = async () => {
     apiClient.get('/adminuser/dashboard/')
       .then((res) => {
         console.log(res.data)
@@ -35,7 +42,29 @@ const HomePage = () => {
 
       })
       .finally(() => setisLoading(false))
-  }, [])
+  }
+
+
+  const fetchData2 = async () => {
+    apiClient.get(`/adminuser/customers/?limit=10&offset=0`)
+      .then((res) => {
+        console.log(res.data)
+        setCustomers(res.data.results)
+        // const result = res.data.count
+        // setTotalPages(Math.ceil(result/limit))
+      })
+      .catch((e) => {
+        console.log(e?.response?.data?.detail)
+        if (e?.response?.data?.detail === "Authentication credentials were not provided.") {
+          toast.error(e?.response?.data?.detail)
+          window.location.replace('/auth/login')
+        }
+        else {
+          toast.error(e?.response?.data?.detail || 'An error Occured')
+        }
+      })
+      .finally(() => setisLoading(false))
+  }
   return (
     <div>
       <div>
@@ -48,27 +77,80 @@ const HomePage = () => {
             <p className='text-[#000000] text-[20px] font-[400]'>Total Customers</p>
             <p className='text-[30px] text-[#000000] font-[600]'>{data.totalCustomers}</p>
           </div>
-          <img src={test1}  className='absolute right-0 bottom-0'></img>
+          <img src={test1} className='absolute right-0 bottom-0'></img>
         </div>
         <div className={`bg-[#fff] lg:w-[300px] sm:w-[200px] h-[150px] rounded-[10px] overflow-hidden shadow-[0px_1px_7.2px_-2px_rgba(0,_0,_0,_0.25)] text-white relative`}>
           <div className='absolute top-[15%] left-[8%] flex flex-col z-[1]'>
             <p className='text-[#000000] text-[20px] font-[400]'>Total Product</p>
             <p className='text-[30px] text-[#000000] font-[600]'>{data.totalProducts}</p>
           </div>
-          <img src={test2}  className='absolute right-0 bottom-0'></img>
+          <img src={test2} className='absolute right-0 bottom-0'></img>
         </div>
         <div className={`bg-[#fff] lg:w-[300px] sm:w-[200px] h-[150px] rounded-[10px] overflow-hidden shadow-[0px_1px_7.2px_-2px_rgba(0,_0,_0,_0.25)] text-white relative`}>
           <div className='absolute top-[15%] left-[8%] flex flex-col z-[1]'>
             <p className='text-[#000000] text-[20px] font-[400]'>Total Order</p>
             <p className='text-[30px] text-[#000000] font-[600]'>{data.totalOrders}</p>
           </div>
-          <img src={test3}  className='absolute right-0 bottom-0'></img>
+          <img src={test3} className='absolute right-0 bottom-0'></img>
         </div>
       </div>
 
-      <div  className='bg-[#fff] mt-8 overflow-hidden rounded-lg shadow-md'>
-        <SalesChart labels={label} series={series} title={"Orders"} />
+      <div className='grid grid-cols-3 gap-6 mt-8'>
+        <div className='bg-[#fff]  overflow-hidden rounded-lg shadow-md'>
+          <SalesChart labels={label} series={series} title={"Orders"} />
+        </div>
+        <div className=" col-span-2 h-full w-full bg-[#fff] p-4 rounded-lg shadow-md ">
+          <div className>
+            <table className="h-full w-full">
+              <thead className="bg-gray-100">
+                <tr className="border-b">
+                  <th className="px-4 py-4 text-start text-sm font-bold text-[#000]">#</th>
+                  <th className="px-4 py-4 text-start text-sm font-bold text-[#000]">First Name</th>
+                  <th className="px-4 py-4 text-start text-sm font-bold text-[#000]">Last Name</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                {customer.map((data, index) => (
+                  <tr
+                    key={index}
+                    className="border-b"
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -20 }}
+                    transition={{ delay: index * 0.1 }}
+                  >
+                    <td className="px-4 py-4 text-start font-semibold text-gray-400 dark:text-gray-400 text-[.9rem] items-center">
+                      {index + 1}
+                    </td>
+                    <td className="px-4 py-4 text-start font-semibold text-gray-400 dark:text-gray-400 text-[.9rem] items-center">
+                      {data.name}
+                    </td>
+                    <td className="px-4 py-4 text-start font-semibold text-gray-400 dark:text-gray-400 text-[.9rem] items-center">
+                      {data.email}
+                    </td>
+                  </tr>
+                ))}
+
+              </tbody>
+
+            </table>
+          </div>
+          <div className='mt-4'>
+            <Link
+              to="/ui/customer/customer-date"
+              className="py-2  text-white font-light tracking-wide bg-[#072D56] rounded-md px-4"
+            >
+              View More
+            </Link>
+          </div>
+
+        </div>
       </div>
+
+
+
+
     </div>
   )
 }
