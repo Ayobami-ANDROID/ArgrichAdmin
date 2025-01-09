0918bf57a3a07acaa3fbfee46ea10f03718977d0
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2025-01-09 13:56:51 +0100

    company code dynamic

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require("tailwindcss/defaultTheme");
+// const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: [
     "./index.html",
@@ -13,7 +13,7 @@ export default {
         xs: "340px",
         xsm: "500px",
         mxl: "1400px",
-        ...defaultTheme.screens,
+        
       },
     extend: {},
   },
